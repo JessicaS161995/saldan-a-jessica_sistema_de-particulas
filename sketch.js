e48d5c7b0ade95d2bab9f353607df7edd7673a5c
JessicaS161995 <ojitos16@outlook.es>
2023-11-06 16:20:41 -0500

    creacion de particulas q mueren

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
-let p;
-
+let sistema = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
-  p = new Particula();
+	createCanvas(windowWidth, windowHeight);
+	angleMode(DEGREES);
 }
 
 function draw() {
-  if (!p.estaMuerta) {
-    p.actualizar();
-    p.mostrar();
-  }
+	background(255, 50);
 
+	let p = new Particula();
+	sistema.push(p);
+
+	for (let i = 0; i < sistema.length; i++) {
+		if (sistema[i].estaMuerta) {
+			sistema.splice(i, 1);
+		}
+	}
+
+	for (let i = 0; i < sistema.length; i++) {
+		sistema[i].actualizar();
+		sistema[i].mostrar();
+	}
+
+	print(sistema.length);
 }
