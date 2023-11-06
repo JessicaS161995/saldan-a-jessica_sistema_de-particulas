2883675cc47fef125adff502af15e44dd985bfd1
JessicaS161995 <ojitos16@outlook.es>
2023-11-06 16:36:01 -0500

    se hizo unos cambios al codigo

--- sketch.js
+++ sketch.js
@@ -6,10 +6,12 @@ function setup() {
 }
 
 function draw() {
-	background(255, 50);
+	background(125, 50, 125);
 
 	let p = new Particula();
-	sistema.push(p);
+  sistema.push(p);
+  
+  background(0, 50);
 
 	for (let i = 0; i < sistema.length; i++) {
 		if (sistema[i].estaMuerta) {
